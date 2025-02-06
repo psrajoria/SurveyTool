96d76e012711a06877352d6a0429336c2ed5916a
Pankaj Rajoria <52934110+psrajoria@users.noreply.github.com>
2025-02-06 16:51:40 +0100

    Update app.py
    added wsgi so commneted  if __name

--- src/app.py
+++ src/app.py
@@ -612,8 +612,8 @@ def calculate_median_similarity():
     return 0
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # with app.app_context():
     #     db.create_all()
    # app.run(debug=False)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+#    app.run(host="0.0.0.0", port=5000, debug=True)
